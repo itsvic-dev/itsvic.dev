7d617a891340a7384aff90f5507d44062a2eabd5
Victor B. <contact@itsvic.dev>
2025-01-11 00:26:27 +0100

    purge everything for a clean mind

--- components/Button.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <button
-    class="px-5 py-2 bg-black hover:bg-bgs-highlight transition-colors text-white rounded"
-  >
-    <slot />
-  </button>
-</template>
