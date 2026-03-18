d49042466e36df3a02e3706bd748523c8550f591
Victor B. <contact@itsvic.dev>
2026-03-19 00:32:19 +0100

    i broke i18n

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,7 +2,7 @@
 const route = useRoute();
 const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection("content")
-    .path(route.path)
+    .path(`/blog/${route.params.slug}`)
     .first();
 });
 
